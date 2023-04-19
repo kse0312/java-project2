ad9bccfea4f9747f0e7ea07037a1776189abb2f6
김승은 <tmddms0317@naver.com>
2023-04-19 14:18:49 +0900

    feat: JavaInput 추가 - InputStreamReader 1문자, 2문자 입출력

---
+++ src/main/java/com/example/javaproject2/Week1/JavaInput.java
@@ -0,0 +1,25 @@
+package com.example.javaproject2.Week1;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class JavaInput {
+    public void readAChar() throws IOException {
+        //InputStreamReader 클래스를 is라는 이름으로 선언
+        InputStreamReader is;
+        //InputStreamReader를 인스턴스화
+        is = new InputStreamReader(System.in);
+        int asciiCode = 0;
+        asciiCode = is.read();
+        System.out.println(asciiCode);
+    }
+    public void readTwoChars() throws IOException {
+        InputStreamReader is;
+        is = new InputStreamReader(System.in);
+        int asciiCode1 = 0;
+        int asciiCode2 = 0;
+        asciiCode1 = is.read();
+        asciiCode2 = is.read();
+        System.out.println(asciiCode1 + " , " + asciiCode2);
+    }
+}
