7e833e6d898f4253ef4caf9f139cbf2431c81348
김승은 <tmddms0317@naver.com>
2023-04-19 15:30:05 +0900

    feat: ScannerEx 생성 - readTwoNumbersAndPlus() 추가 : 숫자합

---
+++ src/main/java/com/example/javaproject2/Week1/ScannerEx.java
@@ -0,0 +1,19 @@
+package com.example.javaproject2.Week1;
+
+import java.util.Scanner;
+
+public class ScannerEx {
+    public void plusTwoNumbers(int val1, int val2) {
+
+    }
+    public  void readTwoNumbersAndPlus(){
+        //숫자 2개 입력 받기
+        Scanner sc = new Scanner(System.in);
+        // 문자열을 처리하기 위해 sc.next() 사용
+        System.out.println(sc.nextInt() + sc.nextInt());
+
+
+    }
+
+
+}
