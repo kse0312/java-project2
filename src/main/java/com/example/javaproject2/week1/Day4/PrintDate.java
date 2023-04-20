6e5b6bd2afa46a8ace98c4d755971f991c890cc4
김승은 <tmddms0317@naver.com>
2023-04-20 11:48:31 +0900

    feat: PrintDate 생성 - 날짜 출력하기

---
+++ src/main/java/com/example/javaproject2/week1/Day4/PrintDate.java
@@ -0,0 +1,7 @@
+package com.example.javaproject2.week1.Day4;
+
+public class PrintDate {
+    public static void main(String[] args) {
+        System.out.printf("%d-%02d-%02d",2023,4,20);
+    }
+}
