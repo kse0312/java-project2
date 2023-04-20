0bfc9a0c6ba96bc043ab20cbc7cdc93a0206e2bb
김승은 <tmddms0317@naver.com>
2023-04-20 11:28:24 +0900

    feat: PrintfTwoValuesShortage, MultiplicationsTable 생성 - printf 학습

---
+++ src/main/java/com/example/javaproject2/week1/Day4/MultiplicationsTable.java
@@ -0,0 +1,14 @@
+package com.example.javaproject2.week1.Day4;
+
+public class MultiplicationsTable {
+    public static void main(String[] args) {
+        System.out.println("2" + "*" + "1" + "=" + "2");
+        System.out.printf("%d * %d = %d", 2, 2, 4);
+//        for(int i = 1;i<10;i++){
+//            for(int j = 1; j<10;j++){
+//                System.out.printf("%d x %d = %02d   ",j,i, i*j);
+//            }
+//            System.out.println("");
+//        }
+    }
+}
---
+++ src/main/java/com/example/javaproject2/week1/Day4/PrintfTwoValuesShortage.java
@@ -0,0 +1,7 @@
+package com.example.javaproject2.week1.Day4;
+
+public class PrintfTwoValuesShortage {
+    public static void main(String[] args) {
+        System.out.printf("%s님 안녕하세요. %s입니다.","김땡땡","박모모");
+    }
+}
