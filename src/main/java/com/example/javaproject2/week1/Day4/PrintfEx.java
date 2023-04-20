4a1afd44b63444f56aa8ace3931444f568c839b9
김승은 <tmddms0317@naver.com>
2023-04-20 11:19:14 +0900

    feat: PrintfEx 생성 - printf 학습

---
+++ src/main/java/com/example/javaproject2/week1/Day4/PrintfEx.java
@@ -0,0 +1,12 @@
+package com.example.javaproject2.week1.Day4;
+
+public class PrintfEx {
+    public void Print(){
+        System.out.printf("%s\n","Hello");
+        System.out.println("Bye");
+    }
+    public static void main(String[] args) {
+        PrintfEx print = new PrintfEx();
+        print.Print();
+    }
+}
