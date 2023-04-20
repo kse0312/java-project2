a46891a7b533922484596819648ab1f062bd9328
김승은 <tmddms0317@naver.com>
2023-04-20 13:37:46 +0900

    feat: Float 실습 + CodeUp1011_1017 추가 - float 실습

--- src/main/java/com/example/javaproject2/codeup/CodeUp1011_1017.java
+++ src/main/java/com/example/javaproject2/codeup/CodeUp1011_1017.java
@@ -2,7 +2,7 @@ package com.example.javaproject2.codeup;
 
 import java.util.Scanner;
 
-public class CodeUp1011_1015 {
+public class CodeUp1011_1017 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 //        String str = sc.next();                                 //1011
@@ -16,6 +16,8 @@ public class CodeUp1011_1015 {
 //        System.out.println(str2 + " " + str1);
 //        float in = sc.nextFloat();                              //1015
 //        System.out.printf("%.2f",in);
+//        int in = sc.nextInt();                                  //1017
+//        System.out.printf("%d %d %d", in, in, in);
 
     }
 }
--- src/main/java/com/example/javaproject2/week1/Day4/Multiplications2.java
+++ src/main/java/com/example/javaproject2/week1/Day4/Multiplications2.java
@@ -2,8 +2,10 @@ package com.example.javaproject2.week1.Day4;
 
 public class Multiplications2 {
     public static void main(String[] args) {
+
         for(int j = 1;j<10;j++){
             System.out.printf("%d * %d = %02d\n", 2, j, 2*j);
+
         }
     }
 }
