b28fd9da158309b6ee3e44d9fd1c3e2899cc0616
김승은 <tmddms0317@naver.com>
2023-04-20 09:39:37 +0900

    feat: ScannerStrEx 생성 - ScannerStrEx 추가 : 문자합

---
+++ src/main/java/com/example/javaproject2/week1/ScannerStrExTest.java
@@ -0,0 +1,10 @@
+package com.example.javaproject2.week1;
+
+
+class ScannerStrExTest {
+    public static void main(String[] args) {
+        ScannerStrEx sc = new ScannerStrEx();
+        sc.inputStringAndPlus();
+    }
+
+}
