385d5c74cfeced44a49448d0f3be9bc05a3d7b3e
김승은 <tmddms0317@naver.com>
2023-04-20 10:05:35 +0900

    feat: CodeUp1006_1010 생성 - CodeUp1006_1010 완료 - Codeup1001_1005 파일 이름 변경 -> CodeUp1001_1005

--- src/main/java/com/example/javaproject2/codeup/CodeUp1001_1005.java
+++ src/main/java/com/example/javaproject2/codeup/CodeUp1001_1005.java
@@ -1,7 +1,7 @@
 package com.example.javaproject2.codeup;
 
 //2023-04-19
-public class Codeup1001_1005 {
+public class CodeUp1001_1005 {
     public static void main(String[] args) {
         //System.out.println("Hello");              //1001
         //System.out.println("Hello World");        //1002
--- src/main/java/com/example/javaproject2/codeup/CodeUp1006_1010.java
+++ src/main/java/com/example/javaproject2/codeup/CodeUp1006_1010.java
@@ -1,17 +1,18 @@
 package com.example.javaproject2.codeup;
 
-import java.util.Scanner;
-
 //2023-04-20
-public class Main {
+public class CodeUp1006_1010 {
     public static void main(String[] args) {
 //        System.out.println("\"!@#$%^&*()\"");                   //1006
 //        System.out.println("\"C:\\Download\\hello.cpp\"\n");    //1007
 //        System.out.println("\u250C\u252C\u2510");               //1008
 //        System.out.println("\u251C\u253C\u2524");
 //        System.out.println("\u2514\u2534\u2518");
-        Scanner sc = new Scanner(System.in);
-        System.out.println(sc.nextInt());
+//        1009번 문제 없음
+//        Scanner sc = new Scanner(System.in);                    //1010
+//        int digits = sc.nextInt();
+//        System.out.println(digits);
+
 
     }
 }
