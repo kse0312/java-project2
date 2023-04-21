1953d30ac03da17e7370ed19a32d6c099de0ac2b
김승은 <tmddms0317@naver.com>
2023-04-21 15:08:01 +0900

    feat: 변수 선언과 초기화 실습 - 23.04.20 수업 자료

---
+++ src/main/java/com/example/javaproject2/week1/Day4/InitializeAVariable.java
@@ -0,0 +1,9 @@
+package com.example.javaproject2.week1.Day4;
+
+public class InitializeAVariable {
+    public static void main(String[] args) {
+        int iVal;   //변수 선언
+        iVal = 1;   //변수 초기화
+        System.out.println(iVal);
+    }
+}
