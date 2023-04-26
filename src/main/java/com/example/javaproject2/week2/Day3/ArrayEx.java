d5b373ebb3758832d34d9d572bfae9bdec2e813a
김승은 <tmddms0317@naver.com>
2023-04-26 10:31:07 +0900

    feat: 2차원 배열 실습 - ArrayEx : 2차원 배열 출력

---
+++ src/main/java/com/example/javaproject2/week2/Day3/ArrayEx.java
@@ -0,0 +1,12 @@
+package com.example.javaproject2.week2.Day3;
+
+import java.util.Arrays;
+
+public class ArrayEx {
+    public static void main(String[] args) {
+        int[][] iArray = new int[10][10];
+        for (int i = 0;i<10;i++){
+            System.out.println(Arrays.toString(iArray[i]));
+        }
+    }
+}
