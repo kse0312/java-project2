ece80195c0141eb6f3c072d2fae8f746261ab820
김승은 <tmddms0317@naver.com>
2023-04-26 15:13:14 +0900

    feat: CodeUp1047_1052

---
+++ src/main/java/com/example/javaproject2/codeup/CodeUp1047_1052.java
@@ -0,0 +1,36 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1047_1052 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+//        System.out.println(sc.nextInt()<<1);            //1047
+
+//        System.out.println(sc.nextInt()<<sc.nextInt()); //1048
+
+//        if(sc.nextInt() > sc.nextInt()){                //1049
+//            System.out.println(1);
+//        }else{
+//            System.out.println(0);
+//        }
+
+//        if(sc.nextInt() == sc.nextInt()){                //1050
+//            System.out.println(1);
+//        }else{
+//            System.out.println(0);
+//        }
+
+//        if(sc.nextInt() <= sc.nextInt()){                //1051
+//            System.out.println(1);
+//        }else{
+//            System.out.println(0);
+//        }
+
+//        if(sc.nextInt() != sc.nextInt()){                //1052
+//            System.out.println(1);
+//        }else{
+//            System.out.println(0);
+//        }
+    }
+}
