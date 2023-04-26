94daf654fdb57012204dd1c40a3fbd3d967fc15d
김승은 <tmddms0317@naver.com>
2023-04-26 11:10:37 +0900

    feat: 2차원 배열 실습

---
+++ src/main/java/com/example/javaproject2/week1/Day3/ArrayXYDiff.java
@@ -0,0 +1,9 @@
+package com.example.javaproject2.week1.Day3;
+
+public class ArrayXYDiff {
+    public static void main(String[] args) {
+        int[][] arr32 = new int[3][2]; // 3*2 형태의 배열
+        int[][] arr67 = new int[6][7]; // 6*7 형태의 배열
+
+    }
+}
