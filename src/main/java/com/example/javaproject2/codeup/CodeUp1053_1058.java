dd4479755b26f3b3c61ae4b74329a7ac2a3e6889
김승은 <tmddms0317@naver.com>
2023-04-27 13:44:41 +0900

    feat: CodeUp1053_1058 완료 + CheckWhetherAdult 실습

---
+++ src/main/java/com/example/javaproject2/codeup/CodeUp1053_1058.java
@@ -0,0 +1,21 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1053_1058 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+//        int val = (sc.nextInt()==1) ? 0 : 1;    //1053
+//        System.out.println(val);
+
+//        System.out.println((sc.nextInt()==1 && sc.nextInt()==1) ? 1:0); //1054
+
+//        System.out.println((sc.nextInt()==1 || sc.nextInt()==1) ? 1:0); //1055
+
+//        System.out.println((sc.nextInt()==1 ^ sc.nextInt()==1) ? 1:0); //1056
+
+//        System.out.println((sc.nextInt()== sc.nextInt()) ? 1:0); //1057
+
+//        System.out.println((sc.nextInt()==0  && sc.nextInt()==0) ? 1:0); //1058
+    }
+}
