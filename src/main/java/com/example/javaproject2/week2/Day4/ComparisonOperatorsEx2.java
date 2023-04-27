adbe7a544582ebd1645cde5880f4d4dd675c443e
김승은 <tmddms0317@naver.com>
2023-04-27 11:03:38 +0900

    feat: 비교 연산자 실습

---
+++ src/main/java/com/example/javaproject2/week2/Day4/ComparisonOperatorsEx2.java
@@ -0,0 +1,14 @@
+package com.example.javaproject2.week2.Day4;
+
+public class ComparisonOperatorsEx2 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.printf("%d == %d --> %b\n", a, b, a == b);
+        System.out.printf("%d != %d --> %b\n", a, b, a != b);
+        System.out.printf("%d < %d --> %b\n", a, b, a < b);
+        System.out.printf("%d <= %d --> %b\n", a, b, a <= b);
+        System.out.printf("%d > %d --> %b\n", a, b, a > b);
+        System.out.printf("%d >= %d --> %b\n", a, b, a >= b);
+    }
+}
