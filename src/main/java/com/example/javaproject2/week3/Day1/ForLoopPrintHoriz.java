6ad7d024f4893b342d77b9dc40855c7eedfe3f1c
김승은 <tmddms0317@naver.com>
2023-05-01 11:28:57 +0900

    feat: for문 실습 - ForLoopPrint : 1~9 세로 출력 - ForLoopPrintHoriz : 1~9 가로 출력 - ForLoopPrintReverse : 10~1,0 출력

---
+++ src/main/java/com/example/javaproject2/week3/Day1/ForLoopPrintHoriz.java
@@ -0,0 +1,9 @@
+package com.example.javaproject2.week3.Day1;
+
+public class ForLoopPrintHoriz {
+    public static void main(String[] args) {
+        for ( int i = 1 ; i < 10 ; i++ ){
+            System.out.print(i);
+        }
+    }
+}
---
+++ src/main/java/com/example/javaproject2/week3/Day1/ForLoopPrintReverse.java
@@ -0,0 +1,14 @@
+package com.example.javaproject2.week3.Day1;
+
+public class ForLoopPrintReverse {
+    public static void main(String[] args) {
+        for (int i=10; i>0 ; i--){
+            System.out.println(i);
+        }
+
+        for (int i=10; i>=0 ; i--){
+            System.out.println(i);
+        }
+
+    }
+}
