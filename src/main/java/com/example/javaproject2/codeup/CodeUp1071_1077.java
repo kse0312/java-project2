89563b2ef55d3af1bb600f4c15497241cd989a93
김승은 <tmddms0317@naver.com>
2023-05-01 11:43:33 +0900

    feat: CodeUp1071_1077 1103_1106 추가

---
+++ src/main/java/com/example/javaproject2/codeup/CodeUp1071_1077.java
@@ -0,0 +1,43 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1071_1077 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+//        int input = 0;                          //1071
+//        while((input = sc.nextInt())!=0){
+//            System.out.println(input);
+//        }
+
+//        int len = sc.nextInt();                 //1072
+//        for(int i = 0 ; i < len ; i++){
+//            System.out.println(sc.nextInt());
+//        }
+
+//        int input = 0;                          //1073
+//        while((input = sc.nextInt())!=0){
+//            System.out.println(input);
+//        }
+
+//        int input = sc.nextInt();               //1174
+//        for(int i = input ; i> 0 ; i--){
+//            System.out.println(i);
+//        }
+
+//        int input = sc.nextInt();               //1175
+//        for(int i = input-1 ; i>= 0 ; i--){
+//            System.out.println(i);
+//        }
+
+//        char input = sc.next().charAt(0);       //1176
+//        for(int i = 97 ; i <= input ; i++) {
+//            System.out.printf("%c ",(char) i);
+//        }
+
+        int input = sc.nextInt();
+        for(int i = 0 ; i <= input ; i++){
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/main/java/com/example/javaproject2/codeup/CodeUp1103_1106.java
@@ -0,0 +1,13 @@
+package com.example.javaproject2.codeup;
+
+public class CodeUp1103_1106 {
+    public static void main(String[] args) {
+//        System.out.println("\"c:\\test\"");     //1103
+
+//        System.out.print(Integer.parseInt("1111111111111111111111111111111",2)+1);          //1106
+//        System.out.print(" ");
+//        System.out.print(Integer.parseInt("1111111111111111111111111111111",2));
+
+
+    }
+}
