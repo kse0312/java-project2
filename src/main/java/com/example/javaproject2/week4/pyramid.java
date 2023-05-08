22d48b100cc8ec7278851ca392406562411d9b92
김승은 <tmddms0317@naver.com>
2023-05-08 10:11:19 +0900

    feat : 피라미드 별 찍기

---
+++ src/main/java/com/example/javaproject2/week4/pyramid.java
@@ -0,0 +1,20 @@
+package com.example.javaproject2.week4;
+
+import java.util.Scanner;
+
+public class pyramid {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        for (int i = 1; i <= input; i++) {
+            for (int j = 0; j < input - i; j++)
+                System.out.printf(" ");
+            for (int j = 0; j < 2 * i - 1; j++) {
+                System.out.printf("*");
+            }
+
+            System.out.printf("\n");
+        }
+
+    }
+}
