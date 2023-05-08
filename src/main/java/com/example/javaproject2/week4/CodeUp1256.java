f9c359bdaa4163c855ac7e16389b3084703fbd2d
김승은 <tmddms0317@naver.com>
2023-05-08 09:25:17 +0900

    feat: CodeUp 문제 풀이 - 1256 : 별찍기 - 1287 : 구구단 별찍기

---
+++ src/main/java/com/example/javaproject2/week4/CodeUp1256.java
@@ -0,0 +1,13 @@
+package com.example.javaproject2.week4;
+
+import java.util.Scanner;
+
+public class CodeUp1256 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        for(int i = 0 ; i < input; i++){
+            System.out.printf("*");
+        }
+    }
+}
