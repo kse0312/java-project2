69cb612496d33d396c36c2ea507dcd13e846c69e
김승은 <tmddms0317@naver.com>
2023-05-08 09:47:41 +0900

    feat: 직각삼각형 별찍기

---
+++ src/main/java/com/example/javaproject2/week4/RightTriangle.java
@@ -0,0 +1,16 @@
+package com.example.javaproject2.week4;
+
+import java.util.Scanner;
+
+public class RightTriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        for(int i = 1 ; i <= input ; i++){
+            for(int j = 0; j < i ; j++){
+                System.out.printf("*");
+            }
+            System.out.printf("\n");
+        }
+    }
+}
